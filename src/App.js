4ec12f49c11bace522b5f9ab5a481e7b78eff871
devipeer <pepe.piotr.pepe@gmail.com>
2021-01-14 00:12:03 +0100

    Need to do registration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React, { useState, useEffect } from 'react'; // useEffect runs a piece on a specific condition
+import React, { useState, useEffect } from 'react'; 
 import './App.css';
 import logo from './logo.png';
 import Post from './Post';
+import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  Button: {
+    color: '#ffffff'
+  }
+}));
+
+const StyledButton = withStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+})(Button);
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
-useEffect(() => {
-  // code runs here 
-}, [posts]
-)
+  useEffect(() => { // useEffect runs a piece on a specific condition
+    // code runs here 
+    db.collection('posts').onSnapshot(snapshot => {
+    // everytime a post is added, this code runs
+      setPosts(snapshot.docs.map(doc => ({ 
+        id: doc.id,
+        post: doc.data()
+        })));
+    })
+  }, []);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const signUp = (event) => {
+
+  }
 
   return (
-    
     <div className="app">
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        {<div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Im a modal</h2>      
+    </div>}
+      </Modal>
+
       <div className="app__header">
         <img className="app__headerImage" src={logo} alt=""/>
+        <StyledButton className="signUpButton" onClick={() => setOpen(true)}>Sign Up</StyledButton>
       </div>
-
+     
       {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        posts.map(({ id, post }) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,3 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC0m-nLB-Hblo6UdjSI6y9B7pLXHOsUofU",
-  authDomain: "arthub-24673.firebaseapp.com",
-  projectId: "arthub-24673",
-  storageBucket: "arthub-24673.appspot.com",
-  messagingSenderId: "1023403557914",
-  appId: "1:1023403557914:web:e943d6a018a68bb681bda3",
-  measurementId: "G-075TGM24Q1"
-};
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
@@ -20,7 +10,7 @@ const firebaseApp = firebase.initializeApp({
   measurementId: "G-075TGM24Q1"
 });
 
-const db = firebaseApp.firestone();
+const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
 
