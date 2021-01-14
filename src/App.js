2b0a50646e2b8bff840404266c4cdc5715d42847
devipeer <pepe.piotr.pepe@gmail.com>
2021-01-14 01:35:46 +0100

    Half done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import logo from './logo.png';
 import Post from './Post';
-import { db } from './firebase';
+import { auth, db } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import { Button } from '@material-ui/core';
+import { Button, Input } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+
 
 function getModalStyle() {
   const top = 50;
@@ -23,13 +24,10 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
     width: 400,
-    backgroundColor: theme.palette.background.paper,
+    backgroundColor: '#fff',
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-  },
-  Button: {
-    color: '#ffffff'
   }
 }));
 
@@ -53,6 +51,36 @@ function App() {
   const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => { // listens for changes on frontend
+    const unsubscribe = auth.onAuthStateChanged((authUser) => { // listens for changes on backend
+      if(authUser) {
+        //user logged in
+        console.log(authUser);
+        setUser(authUser);
+
+        if(authUser.displayName){
+          //dont update username
+        } else {
+          // if we created someone
+          return authUser.updateProfile({
+            displayName: username,
+          });
+        } 
+      } else {
+      //user logged out
+      setUser(null);
+      }
+    })
+    return () => {
+      //perform some clean up
+      unsubscribe();
+    }
+  }, [user, username]);
 
   useEffect(() => { // useEffect runs a piece on a specific condition
     // code runs here 
@@ -70,8 +98,11 @@ function App() {
   };
 
   const signUp = (event) => {
-
-  }
+      event.preventDefault();
+      auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className="app">
@@ -79,14 +110,39 @@ function App() {
         open={open}
         onClose={handleClose}
       >
-        {<div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Im a modal</h2>      
-    </div>}
+        <div style={modalStyle} className={classes.paper}>
+        <form className="app__signup">
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoComplete="off"
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="off"
+            />
+            <Button type="submit" onClick={signUp}>Sign Up</Button>     
+        </form>  
+        </div>
       </Modal>
 
       <div className="app__header">
-        <img className="app__headerImage" src={logo} alt=""/>
-        <StyledButton className="signUpButton" onClick={() => setOpen(true)}>Sign Up</StyledButton>
+        <img className="app__headerImage"
+          src={logo}
+          alt=""
+          />
+        <StyledButton onClick={(signUp) => setOpen(true)}>Sign Up</StyledButton>
       </div>
      
       {
